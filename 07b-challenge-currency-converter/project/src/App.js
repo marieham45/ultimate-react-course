f4560ad94987684307f5cd34a939a749494e87b3
marieham45 <marie.hamsikova@centrum.cz>
2024-02-18 13:46:56 +0100

    07b challenge solution

--- 07b-challenge-currency-converter/project/src/App.js
+++ 07b-challenge-currency-converter/project/src/App.js
@@ -1,22 +1,56 @@
+import {useEffect, useState} from "react";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+    const [amount, setAmount] = useState("")
+    const [from, setFrom] = useState("USD")
+    const [to, setTo] = useState("USD")
+    const [convertedAmount, setConvertedAmount] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+
+
+        async function getConversion() {
+
+            setIsLoading(true)
+            const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`)
+            const data = await res.json()
+            setIsLoading(false)
+            const rate = data.rates[to]
+
+            setConvertedAmount(rate)
+
+
+        }
+
+        if (Number(amount) > 0) {
+            if (from === to) {
+                setConvertedAmount(Number(amount))
+                return
+            } else {
+                getConversion()
+
+            }
+        }
+
+    }, [amount, from, to])
     return (
         <div>
-            <input type="text" />
-            <select>
+            <input disabled={isLoading} value={amount} type="text" onChange={(e) => setAmount(e.target.value)}/>
+            <select disabled={isLoading} value={from} onChange={(e) => setFrom(e.target.value)}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
                 <option value="INR">INR</option>
             </select>
-            <select>
+            <select disabled={isLoading} value={to} onChange={(e) => setTo(e.target.value)}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
                 <option value="INR">INR</option>
             </select>
-            <p>OUTPUT</p>
+            <p>{convertedAmount}</p>
         </div>
     );
 }
