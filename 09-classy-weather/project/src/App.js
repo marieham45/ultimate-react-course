5bd4990bee39cdada2d6601c681b46e8f06cee8b
marieham45 <marie.hamsikova@centrum.cz>
2024-03-05 15:57:30 +0100

    09 custom hook useFetchWeather

--- 09-classy-weather/project/src/App.js
+++ 09-classy-weather/project/src/App.js
@@ -1,60 +1,14 @@
 import Weather from "./components/Weather";
 import Input from "./components/Input";
 import {useEffect, useState} from "react";
+import {useFetchWeather} from "./hooks/useFetchWeather";
 
-
-function convertToFlag(countryCode) {
-    const codePoints = countryCode
-        .toUpperCase()
-        .split("")
-        .map((char) => 127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
-}
 
 
 const App = () => {
     const [location, setLocation] = useState(location => localStorage.getItem("location") || "")
-    const [isLoading, setIsLoading] = useState(false)
-    const [displayLocation, setDisplayLocation] = useState("")
-    const [weather, setWeather] = useState({})
 
-
-    const fetchWeather = async () => {
-        if (location.length < 3) {
-            setWeather({})
-            return
-        }
-
-        try {
-            setIsLoading(true);
-
-            // 1) Getting location (geocoding)
-            const geoRes = await fetch(
-                `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-            );
-            const geoData = await geoRes.json();
-
-            if (!geoData.results) throw new Error("Location not found");
-
-            const {latitude, longitude, timezone, name, country_code} =
-                geoData.results.at(0);
-            setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
-
-            // 2) Getting actual weather
-            const weatherRes = await fetch(
-                `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-            );
-            const weatherData = await weatherRes.json();
-            setWeather(weatherData.daily);
-        } catch (err) {
-            console.error(err);
-        } finally {
-            setIsLoading(false);
-        }
-
-
-    };
-
+    const {isLoading, displayLocation, weather, fetchWeather} = useFetchWeather(location)
 
     useEffect(() => {
 
